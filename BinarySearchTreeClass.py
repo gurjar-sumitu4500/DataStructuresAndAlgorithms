0b901047b36c594d3f1f516ef2a13c183f705a69
Sumit <gurjar.sumitu4500@.com>
2024-05-16 20:06:06 +0530

    BinarySearchTreeClass constructor and insert method

---
+++ BinarySearchTreeClass.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while(True):
+            if new_node.value == temp.value:
+                return False
+            
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+myTree = BinarySearchTree()
+myTree.insert(2)
+myTree.insert(3)
+myTree.insert(1)
+print(myTree.root.value)
+print(myTree.root.left.value)
+print(myTree.root.right.value)
